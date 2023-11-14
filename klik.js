cd3cb0ec5e57fe9656407c9a03e30fdb9acc5e02
mtrekmtrek040405 <147174023+mtrekmtrek040405@users.noreply.github.com>
2023-11-14 19:13:24 +0700

    Add files via upload

---
+++ klik.js
@@ -0,0 +1,17 @@
+
+const btn1 = document.getElementById ('btn1')
+const btn2 = document.getElementById ('btn2')
+
+
+function gantiWarna(){
+    alert ('Saiki namamu sayangkuuu ')
+    alert ('nk koe glm ro aku,chat aku "halo sayang"')
+
+}
+function nambahKata() {
+    const newText = document.createElement ('p')
+    newText.textContent = ('YWSS BABAY ')
+    body.append(newText)
+
+}
+const body= document.body
